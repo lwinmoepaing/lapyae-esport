89307e2190626586fb5abbb9e3bcad31d04401fe
WuWooLay <mamamglay.lp@gmail.com>
2019-07-02 23:22:34 +0630

    Final Update

--- controllers/web/index.js
+++ controllers/web/index.js
@@ -4,31 +4,36 @@ const SHOW_INDEX = (req, res) => {
 			state: '1',
 			name: 'Lwin Moe Paing',
 			team: 'LiverPool',
-			team_img: 'path'
+			team_img: '/images/team/liverpool.png',
+			status: 'Champion'
 		},
 		{
 			state: '2',
 			name: 'Thant Zin Phyo',
 			team: 'LiverPool',
-			team_img: 'path'
+			team_img: '/images/team/liverpool.png',
+			status: '1st RunnerUp'
 		},
 		{
 			state: '3',
 			name: 'Nyi Nyi',
-			team: 'Athetico Madriad',
-			team_img: 'path'
+			team: 'Atletico Madrid',
+			team_img: '/images/team/atletico_madrid.png',
+			status: '2nd RunnerUp'
 		},
 		{
 			state: '4',
 			name: 'Min Htet Kyaw Thu',
 			team: 'Arsenal',
-			team_img: 'path'
+			team_img: '/images/team/arsenal.png',
+			status: '3rd RunnerUp'
 		},
 		{
 			state: '5',
 			name: 'Aung Myo Kyaw',
 			team: 'Juventus',
-			team_img: 'path'
+			team_img: '/images/team/juventus.png',
+			status: '4th RunnerUp'
 		}
 	]
 	res.render('home/index', {
