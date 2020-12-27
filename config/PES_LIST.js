4bee0861e76bde692c56ef9e271a3e77919a6094
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-12-27 18:42:32 +0630

    Update Active User

--- config/PES_LIST.js
+++ config/PES_LIST.js
@@ -48,30 +48,6 @@ const getPesWinnerList = async () => {
             throphy_status: 'winner',
             status: 'Champion'
         },
-        {
-            ... getUserById('tzp', users),
-            ... getTeamById('liverpool', teams),
-            throphy_status: 'first',
-            status: '1st RunnerUp'
-        },
-        {
-            ... getUserById('nyi', users),
-            ... getTeamById('atletico_madrid', teams),
-            throphy_status: 'second',
-            status: '2nd RunnerUp'
-        },
-        {
-            ... getUserById('mhkt', users),
-            ... getTeamById('arsenal', teams),
-            throphy_status: 'third',
-            status: '3rd RunnerUp'
-        },
-        {
-            ... getUserById('amk', users),
-            ... getTeamById('juventus', teams),
-            throphy_status: 'best',
-            status: '4th RunnerUp'
-        }
     ]
 
     return data 
