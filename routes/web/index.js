ab3128423e58b77b54206a4f27ec198cad2a0d07
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-12-31 15:37:59 +0630

    Update PPP

--- routes/web/index.js
+++ routes/web/index.js
@@ -7,6 +7,8 @@ router.get('/players', SHOW_USERS)
 
 router.get('/players/:player_id', SHOW_PLAYER_BY_ID)
 
+router.get('/throphy_players', SHOW_ALL_BLOGS)
+
 router.get('/blog/:blog_id', SHOW_BLOG_BY_ID)
 
 router.get('/blog', SHOW_ALL_BLOGS)
