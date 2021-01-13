424b6ade77f001e8f6efd78b88ebb04c54b3e147
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2021-01-13 11:30:12 +0630

    Update Pubg

--- routes/web/index.js
+++ routes/web/index.js
@@ -7,11 +7,11 @@ router.get('/players', SHOW_USERS)
 
 router.get('/players/:player_id', SHOW_PLAYER_BY_ID)
 
-router.get('/throphy_players', SHOW_ALL_THROPHY_PLAYERS)
+router.get('/blog', SHOW_ALL_THROPHY_PLAYERS)
 
 router.get('/blog/:blog_id', SHOW_BLOG_BY_ID)
 
-router.get('/blog', SHOW_ALL_BLOGS)
+router.get('/throphy_players', SHOW_ALL_BLOGS)
 
 module.exports = app => {
 	app.use('/', router)
